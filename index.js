4d76a24bcbbbf64b13241f73faaf24898ad605fd
Vherra <vherragalsim030@gmail.com>
2024-07-06 10:08:39 +0800

    Update index.js

--- index.js
+++ index.js
@@ -62,16 +62,16 @@ function addOrder(){
         totalPrice += parseFloat(qty6.value) * parseFloat(price6.textContent);
     }
 
-    total.value = '₱' + totalPrice.toFixed(2);
+    total.value = '₱ ' + totalPrice.toFixed(2);
     calculateChange();
 }
 
 function calculateChange() {
-    let totalPrice = parseFloat(total.value.replace('₱', ''));
+    let totalPrice = parseFloat(total.value.replace('₱ ', ''));
     let cashTendered = parseFloat(cash.value);
     if (!isNaN(totalPrice) && !isNaN(cashTendered) && cashTendered >= totalPrice) {
         let changeAmount = cashTendered - totalPrice;
-        change.value = '₱' + changeAmount.toFixed(2);
+        change.value = '₱ ' + changeAmount.toFixed(2);
     } else {
         change.value = '';
     }
